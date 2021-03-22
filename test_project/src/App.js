9e022daf886d97c90e0733c7ec2dade386b91a23
mohnishbhamra <mohnishsinghbhamra@google.com>
2021-03-23 00:56:57 +0530

    First Class component

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import './components/FirstComponent'
 import Hello from './components/FirstComponent';
+import Welcome from './components/MyClassComponent';
+
 function App() {
   return (
     <div className="App">
       <Hello name="Mohnish" lastName="Bhamra"></Hello>
       <p>The pragraph which is automatically the children property</p>
+
+      <Welcome></Welcome>
     </div>
   );
 }
